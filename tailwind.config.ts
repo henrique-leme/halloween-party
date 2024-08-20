60696ca5971f8c6f0a39e1524e0dac0f5387542c
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-08-20 17:37:05 -0300

    feat: initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,23 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        black: '#0d0d0d',
+        red: '#b11226',
+        wine: {
+          600: '#6b0f1a',
+          700: '#5a0915',
+        },
+        gray: {
+          200: '#e5e5e5',
+          300: '#c5c5c5',
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
