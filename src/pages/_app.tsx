ba7cfcf2e54458d970bf1bea270e85d396cbee5c
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-08-22 22:53:15 -0300

    feat: add prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import '../styles/globals.css'
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
       <Footer />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default App
